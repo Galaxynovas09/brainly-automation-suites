285d7891204edfc0c7f661dfd73f52ade0825954
Galaxynovas09 <glaxyserver@gmail.com>
2025-10-24 13:50:17 +0300

    panel.user.js

--- panel.user.js
+++ panel.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Brainly Moderation Panel PLUS5 (Elegant UI + Mobile Right Fixed Scrollable Policy)
 // @namespace    http://tampermonkey.net/
-// @version      3.3
+// @version      3.5
 // @description  Roma formu moderasyon paneli 
 // @match        *://*/*
 // @grant        none
